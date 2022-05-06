7e9509ad79535d876c9f68c4b0a95f16b15510f4
taskinmustafa <30447937+taskinmustafa@users.noreply.github.com>
2022-05-07 01:35:28 +0300

    Add files via upload
    code prettify

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <SDL2/SDL_pixels.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
@@ -28,31 +29,14 @@ void voronoi(int point_count, int w, int h, SDL_Renderer *renderer){
     int col=0;
     for (int i=0; i<w; i++){
         for (int j=0; j<h; j++) {
+            SDL_Color color_arr[4] = {  {0,255,255,255},
+                                        {0,0,255,255},
+                                        {0,255,0,255},
+                                        {255,255,0,255}};
+            
             SDL_Point p = {i,j};
-            col = find_closest_manhattan(p, points, point_count);
-            if (col == 0) {
-                SDL_SetRenderDrawColor( renderer,0, 255, 255, 255);     //cyan
-            }else if (col == 1) {
-                SDL_SetRenderDrawColor( renderer,0, 0, 255, 255);       //blue
-            }else if (col == 2) {
-                SDL_SetRenderDrawColor( renderer,0, 255, 0, 255);       //green
-            }else if (col == 3) {
-                SDL_SetRenderDrawColor( renderer,255, 255, 0, 255);     //yellow
-            }else if (col == 4) {   
-                SDL_SetRenderDrawColor( renderer,128, 0, 128, 255);     //purple
-            }else if (col == 5) {
-                SDL_SetRenderDrawColor( renderer,74, 128, 77, 255);     //petrol green
-            }else if (col == 6) {
-                SDL_SetRenderDrawColor( renderer,255, 203, 219, 255);   //pink
-            }else if (col == 7) {
-                SDL_SetRenderDrawColor( renderer,165, 42, 42, 255);     //brown
-            }else if (col == 8) {
-                SDL_SetRenderDrawColor( renderer,238, 236, 223, 255);   //marble
-            }else if (col == 9) {
-                SDL_SetRenderDrawColor( renderer,255, 127, 0, 255);     //orange
-            }else if (col == 10) {
-                SDL_SetRenderDrawColor( renderer,255, 0, 0, 255);       //red
-            }
+            col = find_closest_manhattan(p, points, point_count) % 4;
+            SDL_SetRenderDrawColor(renderer, color_arr[col].r, color_arr[col].g, color_arr[col].b, color_arr[col].a);
             SDL_RenderDrawPoint(renderer, i, j);
         }
 
@@ -66,7 +50,7 @@ int main(int argc, char **argv){
     SDL_Init(SDL_INIT_EVERYTHING);
     int w,h,point_count;
     w = 1280; h = 720;
-    point_count=11; //up to 10
+    point_count=16; //up to 4
     SDL_Window *window = SDL_CreateWindow(  "titleeeeee",
                                             SDL_WINDOWPOS_UNDEFINED,
                                             SDL_WINDOWPOS_UNDEFINED,
@@ -77,15 +61,10 @@ int main(int argc, char **argv){
     SDL_RenderClear(renderer); //set background
 
     voronoi(point_count, w, h, renderer);
-   /*
-    for (int i=0; i<point_count; i++) { //some debugging
-        SDL_SetRenderDrawColor( renderer,0, 0, 0, 255);
-        SDL_RenderDrawPoint(renderer,points[i].x,points[i].y);
-    }*/
     SDL_RenderPresent(renderer);
 
 
-    SDL_Delay(50000); 
+    SDL_Delay(5000); 
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
